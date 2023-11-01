586ab04c009e8d3bf07c2984578ebc07cf8b1e04
Stungler <47352800+StayFN@users.noreply.github.com>
2023-11-01 10:44:14 +0100

    Created CompanyReaderClass

---
+++ Java/IdentityResolution/src/main/java/CompanyReader/CompanyXMLReader.java
@@ -0,0 +1,18 @@
+package CompanyReader;
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.List;
+import java.util.Locale;
+
+import org.w3c.dom.Node;
+
+import de.uni_mannheim.informatik.dws.winter.model.DataSet;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
+
+public class CompanyXMLReader {
+}
