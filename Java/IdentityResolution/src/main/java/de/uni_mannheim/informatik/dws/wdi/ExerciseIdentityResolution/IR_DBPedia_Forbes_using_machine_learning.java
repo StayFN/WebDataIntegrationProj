5c749d11216745b2f3e92a9b4e0998dfeeae967a
Stungler <stfnstingl@gmail.com>
2023-11-15 18:00:23 +0100

    Changed value to 0.3 more sane rsults (no precision 1.0)

--- Java/IdentityResolution/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_DBPedia_Forbes_using_machine_learning.java
+++ Java/IdentityResolution/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_DBPedia_Forbes_using_machine_learning.java
@@ -62,7 +62,7 @@ public class IR_DBPedia_Forbes_using_machine_learning {
 		// create a matching rule
 		String options[] = new String[] { "-S" };
 		String modelType = "SimpleLogistic"; // use a logistic regression
-		WekaMatchingRule<Company, Attribute> matchingRule = new WekaMatchingRule<>(0.4, modelType, options);
+		WekaMatchingRule<Company, Attribute> matchingRule = new WekaMatchingRule<>(0.30, modelType, options);
 		matchingRule.activateDebugReport("data/output/debugResultsMatchingRule.csv", 1000, gsDbpedia_Forbes);
 		
 		// add comparators
